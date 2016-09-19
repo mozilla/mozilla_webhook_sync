45e71d3e3c1e736055c75e1c66394ce4bdebaff2
Walter Lau <walter@fissionstrategy.com>
2016-09-19 15:50:10 -0700

    adding escape function

--- apis/sf_backends_staging.py
+++ apis/sf_backends_staging.py
@@ -1,7 +1,7 @@
 from simple_salesforce import Salesforce
 import requests
 from django.conf import settings
-import json
+import re
 
 def get_sf_session():
     session = requests.Session()
@@ -54,13 +54,13 @@ def fetch_campaign(object_id):
 
 def fetch_campaign_by_name(name):
     sf = get_sf_session()
-    query = "select id from Campaign where Name = '{0}'".format(json.dumps(name))
+    query = "select id from Campaign where Name = '{0}'".format(re.sub(r"([\'])", r'\\\1', name))
     return sf.query_all(query)
 
 
 def insert_campaign(object):
     sf = get_sf_session()
-    query = "select id from Campaign where Name = '{0}'".format(json.dumps(object['Name']))
+    query = "select id from Campaign where Name = '{0}'".format(re.sub(r"([\'])", r'\\\1', object['Name']))
     results = sf.query_all(query)
     try:
         object_id = results['records'][0]['Id']
@@ -78,7 +78,7 @@ def upsert_contact_to_campaign(object):
     sf = get_sf_session()
 
     # search for existing user
-    query = "select id from CampaignMember where ContactId = '{0}' and CampaignId = '{1}'".format(object['ContactId'], json.dumps(object['CampaignId']))
+    query = "select id from CampaignMember where ContactId = '{0}' and CampaignId = '{1}'".format(object['ContactId'], re.sub(r"([\'])", r'\\\1', object['CampaignId']))
     results = sf.query_all(query)
     try:
         object_id = results['records'][0]['Id']
@@ -102,7 +102,7 @@ def fetch_campaign_member(object_id):
 def upsert_campaign(object):
     sf = get_sf_session()
 
-    query = "select id from Campaign where Name = '{0}'".format(json.dumps(object['Name']))
+    query = "select id from Campaign where Name = '{0}'".format(re.sub(r"([\'])", r'\\\1', object['Name']))
     results = sf.query_all(query)
 
     try:
