ddc93cb099cef4159a34895060b38693181dec55
Walter Lau <walter@fissionstrategy.com>
2016-09-23 17:18:08 -0700

    making sync_time null and blank to False

--- events/models.py
+++ events/models.py
@@ -15,9 +15,9 @@ class Campaign(models.Model):
     type = models.CharField(max_length=255, default='Event')
     creator_sf_id = models.CharField(max_length=255, blank=True, null=True)
     content = models.TextField(blank=True, null=True)
-    sync_time = models.DateTimeField(default=timezone.now, blank=True, null=True)
+    sync_time = models.DateTimeField(default=timezone.now)
     parent_id = models.CharField(max_length=255, default=settings.EVENT_PARENT_ID)
-    email_opt_in = models.BooleanField(default=False)
+    # email_opt_in = models.BooleanField(default=False)
 
 
 class CampaignMember(models.Model):
