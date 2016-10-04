14389ba1d8ea77a5d8415f46433bd87c1fdfc654
Walter Lau <walter@fissionstrategy.com>
2016-10-04 16:13:32 -0700

    adding delete logs custom command

---
+++ nb_hook/management/commands/delete_logs.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+from nb_hook.models import *
+from datetime import datetime, timedelta
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        print("Removing old logs more than 48 hours ago...")
+
+        date_from = datetime.now() - timedelta(days=2)
+        old_logs = Log.objects.filter(created_at___lte=date_from)
+
+        for log in old_logs:
+            print log.created_at
