832f3da5d6b97bcc5fb1127216a23ea0890d1079
Walter Lau <walter@fissionstrategy.com>
2016-10-04 16:39:28 -0700

    set delete logs lte date to 1

--- nb_hook/management/commands/delete_logs.py
+++ nb_hook/management/commands/delete_logs.py
@@ -6,7 +6,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print("Removing old logs more than 48 hours ago...")
 
-        date_from = datetime.now() - timedelta(days=2)
+        date_from = datetime.now() - timedelta(days=1)
         old_logs = Log.objects.filter(created_at__lte=date_from)
 
         for log in old_logs:
