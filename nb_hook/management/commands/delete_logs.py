d76b8f6aac1a3392990328667b4d1b9db6237cce
Walter Lau <walter@fissionstrategy.com>
2016-10-04 16:46:38 -0700

    set delete lte to 12 hrs

--- nb_hook/management/commands/delete_logs.py
+++ nb_hook/management/commands/delete_logs.py
@@ -6,7 +6,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print("Removing old logs more than 48 hours ago...")
 
-        date_from = datetime.now() - timedelta(days=1)
+        date_from = datetime.now() - timedelta(hours=12)
         print date_from
         Log.objects.filter(created_at__lte=date_from).delete()
 
