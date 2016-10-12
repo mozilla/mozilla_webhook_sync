02318f202c84f0cdfc807c3b8b857119df18ed19
Walter Lau <walter@fissionstrategy.com>
2016-10-12 11:05:27 -0700

    remove 30 minute sync limit

--- events/sync.py
+++ events/sync.py
@@ -90,8 +90,8 @@ def fetch_save_event(event):
             return False
     else:
         # if event was updated less than 30 minutes ago, skip it
-        if event_dj.sync_time > timezone.now() - timezone.timedelta(minutes=30):
-            return False
+        # if event_dj.sync_time > timezone.now() - timezone.timedelta(minutes=30):
+        #     return False
 
         event_nb = nb_backends.fetch_event(event_dj.nb_id).json()
         if event_nb != event:
