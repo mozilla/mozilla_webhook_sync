77fa0bcdd7ee336eb3e6f1e02bdbcdd2d7c57d9d
Walter Lau <walter@fissionstrategy.com>
2016-10-12 12:38:28 -0700

    fixing language

--- events/sync.py
+++ events/sync.py
@@ -170,6 +170,7 @@ def compare_nb_dj_member_list(nb_list):
         print nb_member['person_id']
         print nb_member['event_id']
         if not CampaignMember.objects.filter(member_nb_id=nb_member['person_id'], campaign_id_id=nb_member['event_id']).exists():
+            print 'not exists'
             user_details = nb_backends.fetch_user(nb_member['person_id']).json()
             event_dj = get_object_or_None(Campaign, nb_id=nb_member['event_id'])
 
