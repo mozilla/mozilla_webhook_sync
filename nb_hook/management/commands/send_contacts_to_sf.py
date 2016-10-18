06851dd4f4bca39cb92320838d5b0b6d3d449818
Walter Lau <walter@fissionstrategy.com>
2016-10-18 16:52:54 -0700

    add sync id_lte, (the last ID before it stops in 9/29/2016)

--- nb_hook/management/commands/send_contacts_to_sf.py
+++ nb_hook/management/commands/send_contacts_to_sf.py
@@ -15,7 +15,7 @@ class Command(BaseCommand):
         print("Begin sending contacts to Salesforce")
 
         # fetch un-synced contacts from database
-        contact_list = ContactSync.objects.filter(synced=False).order_by('created_at')[:10]
+        contact_list = ContactSync.objects.filter(synced=False, id__lte=58512).order_by('created_at')[:1500]
         # sync them to salesforce
         for contact in contact_list:
             if not check_count():
